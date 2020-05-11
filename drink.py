8c36522eb2783d55cb87da2cfc0c07b2e4390bbb
Siddhant kumar Mishra <47280736+Siddhantk123@users.noreply.github.com>
2020-05-11 12:49:04 +0530

    Update drink.py

--- drink.py
+++ drink.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
             #attributes
             title="**Please drink water now!",
             message="Its time to drink water again. its been an hour you didnot drink water",
-            #app_icon="",
+            app_icon="icon.ico",
             timeout=12
             )
         time.sleep(60*60)    
